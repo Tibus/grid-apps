db2e4d0df5f7c88fe3d6aad1814607438e7d4231
julien delnatte <julien.delnatte@gmail.com>
2022-01-27 22:59:28 +0100

    fix publish script

--- scripts/publish.js
+++ scripts/publish.js
@@ -10,7 +10,7 @@ function log(message, color = "\x1b[33m%s\x1b[0m") {
 }
 
 async function execute(...command) {
-  
+
   await new Promise((res) => {
     const { spawn } = require("child_process");
     command = command.join(" && ");
@@ -35,21 +35,16 @@ async function execute(...command) {
 
 (async () => {
   const VERSION = require('../package.json').version;
-  
+
   log("VERSION : "+VERSION, "\x1b[34m");
-  
+
   await execute("npm install");
   await execute("git add --all");
   await execute("git diff-index --quiet HEAD || git commit -m \"Bump " + VERSION + "\"");
-  await execute("git push");
-  await execute("git switch master");
-  await execute("git merge develop");
   await execute("git tag " + VERSION + "");
   await execute("git push");
   await execute("git push --tags");
   await execute("npm publish");
-  await execute("git switch develop");
-  await execute("git merge master");
-  
+
   log("UPDATE COMPLETED", "\x1b[34m");
 })()
